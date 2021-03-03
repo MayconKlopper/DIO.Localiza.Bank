f20a5401598047585370e0ec828b513e035ed3ab
Maycon Klopper de Carvalho <mayconcarvalho@brq.com>
2021-03-02 21:43:06 -0300

    Adicionar arquivos de projeto.

---
+++ DIO.Localiza.Bank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DIO.Localiza.Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
